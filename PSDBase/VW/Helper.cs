10a5fc7f61fbc364ffb641c58cce55642234872d
Palome06 (Gemini) <Palome06@yahoo.com>
2016-05-02 00:09:57 +0800

    Use buffered BinaryReader/BinaryWriter to cut the package size
    Needed to replace the sent/recv one by one instead of using dict

--- PSDBase/VW/Helper.cs
+++ PSDBase/VW/Helper.cs
@@ -3,49 +3,58 @@ using System.IO;
 using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using System.Collections.Generic;
 
 namespace PSD.Base.VW
 {
     public static class WHelper
     {
+        private static IDictionary<NetworkStream, BinaryReader> rTable =
+            new Dictionary<NetworkStream, BinaryReader>();
+        private static IDictionary<NetworkStream, BinaryWriter> wTable =
+            new Dictionary<NetworkStream, BinaryWriter>();
         // public const int MSG_SIZE = 4096;
         // Read from Socket Tunnel
-        public static string ReadByteLine(NetworkStream ns)
+        public static string ReadByteLine(BinaryReader br)
         {
             try
             {
-                byte[] byteInt = new byte[sizeof(int)];
-                // read raw size from the remote stream
-                ns.Read(byteInt, 0, sizeof(int));
-                // convert it into network form
-                int clenN2H = BitConverter.ToInt32(byteInt, 0);
-                // convert in into local host form
-                int clen = IPAddress.NetworkToHostOrder(clenN2H);
-                // read actual messge string
-                byte[] actual = new byte[clen];
-                ns.Read(actual, 0, clen);
-                // convert byte arrya to string
-                return clen > 0 ? Encoding.Unicode.GetString(actual, 0, clen) : null;
+                return br.ReadString();
             }
             catch (IOException) { return ""; }
         }
         // Write into Socket Tunnel
-        public static void SentByteLine(NetworkStream ns, string value)
+        public static void SentByteLine(BinaryWriter bw, string value)
         {
-            // copy string to a byte array
-            byte[] dataArray = Encoding.Unicode.GetBytes(value);
-            // get string length
-            int reqLen = dataArray.Length;
-            // convert string length value to network order
-            int reqLenH2N = IPAddress.HostToNetworkOrder(reqLen);
-            // get string length value into a byte array
-            byte[] reqLenArray = BitConverter.GetBytes(reqLenH2N);
-            // send the length value
-            ns.Write(reqLenArray, 0, sizeof(int));
-            // send the string array
-            ns.Write(dataArray, 0, reqLen);
-            // flush the stream
-            ns.Flush();
+            try
+            {
+                bw.Write(value);
+                bw.Flush();
+            }
+            catch (IOException) { }
+        }
+
+        public static string ReadByteLine(NetworkStream nw)
+        {
+            if (!rTable.ContainsKey(nw))
+            {
+                BinaryReader br = new BinaryReader(nw);
+                rTable[nw] = br;
+                return ReadByteLine(br);
+            }
+            else
+                return ReadByteLine(rTable[nw]);
+        }
+        public static void SentByteLine(NetworkStream nw, string value)
+        {
+            if (!wTable.ContainsKey(nw))
+            {
+                BinaryWriter bw = new BinaryWriter(nw);
+                wTable[nw] = bw;
+                SentByteLine(bw, value);
+            }
+            else
+                SentByteLine(wTable[nw], value);
         }
     }
 }
