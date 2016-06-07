ae8860ff6829f002cb595f70ab84b67edf6d5b25
Palome06 (Gemini) <Palome06@yahoo.com>
2016-06-07 21:39:07 +0800

    support staging port env for network code

--- PSDBase/NetworkCode.cs
+++ PSDBase/NetworkCode.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
 
 namespace PSD.Base
 {
-    public class NetworkCode
+    public static class NetworkCode
     {
-        public const int DIR_PORT = 40201;
+        public static int DIR_PORT;
+        public static int HALL_PORT;
 
-        public const int HALL_PORT = 40421;
+        static NetworkCode()
+        {
+            if (File.Exists("PSDDorm.AKB48Show!"))
+            {
+                DIR_PORT = 41201; HALL_PORT = 41421;
+            }
+            else
+            {
+                DIR_PORT = 40201; HALL_PORT = 40421;
+            }
+        }
     }
 }
